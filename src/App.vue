571c25a635cfb9f3a8abfd9153e3d925b198a8f7
Firlans <firlansyah54321@gmail.com>
2025-10-05 18:31:08 +0700

    starting project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BaseButton from '@/components/base/BaseButton.vue'
+function clicked() {
+  console.log('clicked')
+}
+</script>
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <BaseButton text="  clicked" icon="fa-solid fa-download" :onClick="clicked"></BaseButton>
+</template>
+
+<style scoped></style>
---
+++ src/components/features/CartFeature.vue
@@ -0,0 +1,3 @@
+<template>
+  <a href="#contact" class="cta-button">Get in touch</a>
+</template>
