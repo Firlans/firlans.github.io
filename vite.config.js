042a038e443a5b1d693462d81444182b527640e5
Firlans <firlansyah54321@gmail.com>
2025-10-16 23:30:58 +0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: '/my-portfolio/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
